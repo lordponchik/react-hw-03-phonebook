17f982d360ac1af67d19b8e18cf7711f1c0b1323
Denys Strashniuk <79088186+lordponchik@users.noreply.github.com>
2024-03-26 19:18:55 +0100

    add localstorage for contacts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,17 +6,28 @@ import Filter from './Filter/Filter';
 import ContactList from './ContactList/ContactList';
 import Notification from './Notification/Notification';
 
+const CONTACTS = 'contacts';
+
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
+  componentDidMount() {
+    const localData = localStorage.getItem(CONTACTS);
+
+    if (localData)
+      this.setState({
+        contacts: JSON.parse(localData),
+      });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem(CONTACTS, JSON.stringify(this.state.contacts));
+    }
+  }
   handleFilter = evt => {
     const { value } = evt.target;
 
